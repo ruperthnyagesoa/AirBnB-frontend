091a7d189bf42aecc63dbd37112b0f8b2f5f2468
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-06-10 18:02:16 +0300

    feat:created new routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React,{ useState, useEffect } from 'react';
+import { Route, Routes,useNavigate,useParams } from "react-router"
+import Header from './Header'
+import Destinations from './Destinations'
+import Home from './Home'
+import Trips from './Trips'
+import Stops from './Stops'
+import Destination from './Destination'
+import Trip from './Trip'
+import Stop from './Stop'
+import NewDestination from './NewDestination';
+import NewTrip from './NewTrip'
+import NewStop from './NewStop'
+
 
 function App() {
+  let navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Header />
+    <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/destinations" element={<Destinations/>} />
+            <Route path="/destinations/:id" element={<Destination/>} />
+            <Route path="/destinations/new" element={<NewDestination/>} />
+            <Route path="/trips" element={<Trips/>} />
+            <Route path="/trips/:id" element={<Trip/>} />
+            <Route path="/trips/:id/new_stop" element={<NewStop/>} />
+            <Route path="/trips/new" element={<NewTrip/>} />
+            {/* <Route path="/stops" element={<Stops/>} /> */}
+            <Route path="/stops/:id" element={<Stop/>} />
+          </Routes>
+</div>
   );
 }
 
