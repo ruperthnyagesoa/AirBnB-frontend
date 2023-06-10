e38dc4d6e9b3adf56bd4bbd218a7f1f6ff3489c2
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-06-10 18:50:51 +0300

    feat:updated Home.js;deleted

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import Image from 'react-bootstrap/Image'
 function Home(){
     return(
         <div>
-            <p>Plan your next vacation!!</p>
             <Image src='https://thepointsguy.global.ssl.fastly.net/us/originals/2021/07/Glacier-National-Park_HaizhanZheng.jpg'/>
         </div>
     )
